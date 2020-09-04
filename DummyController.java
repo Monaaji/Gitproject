d86f7cefa669a9aaffac1df16e1d4f6afffd64bc
Monaaji <ajaysengottuvel@gmail.com>
2020-09-04 08:22:35 +0530

    Testing triggers

--- DummyController.java
+++ DummyController.java
@@ -12,6 +12,6 @@ public class DummyController {
 	@RequestMapping(value="/hello/{a}",method=RequestMethod.GET)
 	 public String getHello(@PathVariable("a") int a)
 	 {
-		return "Hello!!!!! " + a;
+		return "Hello WOrld !!!!! " + a;
 	 }
 }	
